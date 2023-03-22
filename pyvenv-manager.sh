539bdd86653045866282ae8b0aadd8b4daf9a7ce
Anuj Datar <anuj.datar@gmail.com>
2023-03-22 19:18:03 +0530

    Added proper venv create, detect and activate scripts

--- pyvenv-manager.sh
+++ pyvenv-manager.sh
@@ -5,23 +5,26 @@ PYVENV_MGR_VERSION="0.0.1"
 windows() { [ -n "$WINDIR" ]; }
 
 create() {
-
-  if [ "$1" ]; then
-    target_dir="$1"
+  if [ -z "$1" ]; then
+    target_dir="$HOME/.pyvenvs/venvs/$(basename pwd)"
+  elif [ "$1" == "--local" ]
+    target_dir="venv"
   else
-    target_dir="$(basename pwd)"
+    target_dir="$HOME/.pyvenvs/venvs/$1"
   fi
-  python3 -m venv "$HOME/.pyvenvs/$1"
+  python3 -m venv "$target_dir"
 }
 
 venv_detect() {
-  if [ "$1" ]; then
-    env_dir="$HOME/.pyvenvs/$1"
+  env_dir="$1"
+
+  if windows; then
+    py_exe="$env_dir/Scripts/python"
   else
-    env_dir="$(pwd)/venv"
+    py_exe="$env_dir/bin/python"
   fi
 
-  if [ -f "$env_dir/bin/python" ]; then
+  if [ -f "$py_exe" ]; then
     echo "Python virtual environment available at $env_dir"
   else
     echo "No Python virtual environment available at $env_dir"
@@ -30,15 +33,20 @@ venv_detect() {
 }
 
 activate() {
-  if [ "$1" ]; then
-    if windows; then
-      . $/Scripts/activate
-    else
-      . $1/bin/activate
-    fi
+  if [ -d "$(pwd)/venv" ]; then
+    env_dir="$(pwd)/venv"
+  elif [ -z "$1" ]; then
+    env_dir="$HOME/.pyvenvs/venvs/$(basename pwd)"
   else
-    echo "Python virtual environment not provided"
-    exit 1
+    env_dir="$HOME/.pyvenvs/venvs/$1"
+  fi
+
+  venv_detect $env_dir
+
+  if windows; then
+    . $env_dir/Scripts/activate
+  else
+    . $env_dir/bin/activate
   fi
 }
 
@@ -46,6 +54,12 @@ manager() {
   case $1 in
     "--version" | "-v" )
       echo $PYVENV_MGR_VERSION;;
+    "create")
+      shift
+      create $@;;
+    "activate")
+      shift
+      activate $@;;
   esac
 }
 
